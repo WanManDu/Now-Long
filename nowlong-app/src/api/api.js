1546488f869e739cb770fd41ecc90f72a8c46bde
WanManDu <wangyu7958@gmail.com>
2025-04-29 22:49:29 +0900

    Init

--- nowlong-app/src/api/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-import { API_BASE_URL, FIREBASE_ID_TOKEN } from '@env';  
-
-export const api = axios.create({
-  baseURL: API_BASE_URL,
-  headers: {
-    Authorization: `Bearer ${FIREBASE_ID_TOKEN}`, 
-  },
-});
--- nowlong-app/src/constants/Colors.js
+++
@@ -1,6 +0,0 @@
-export default {
-    primary: '#007AFF',
-    background: '#F5F5F5',
-    text: '#333333',
-  };
-  
